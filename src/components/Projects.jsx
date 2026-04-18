806c9eedb56b1481334d32bc2d97260f2727a43b
Chris Cottingham <chris@ccotti.com>
2026-04-18 15:12:43 -0500

    testing photo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ function Projects() {
         <div className="projects-grid">
 
           <div className="project-card">
-           <img src="/task-manager.png" alt="test" />
+           <img src="https://picsum.photos/600/300" />
 
             <h3>Task Manager App</h3>
             
