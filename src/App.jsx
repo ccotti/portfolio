739e4f4b7839a7d0fb7e0363f9caf04d4d45661b
Chris Cottingham <chris@ccotti.com>
2026-04-20 15:54:41 -0500

    weather app

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Contact from "./components/Contact";
 import WeatherApp from "./components/WeatherApp";
-import "./styles.css";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <Projects />
-      <Skills />
-      <Contact />
-      <WeatherApp />
-    </>
-  );
+  return <WeatherApp />;
 }
 
 export default App;
